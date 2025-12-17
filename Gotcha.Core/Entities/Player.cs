9ff38033b2fb24813acf8c3244ac79266e0a130b
Mathijs Slabbinck <mathijs.slabbinck@student-howest.be>
2025-12-17 19:55:12 +0100

    Finish Player entity #4

--- Gotcha.Core/Entities/Player.cs
+++ Gotcha.Core/Entities/Player.cs
@@ -21,6 +21,8 @@ namespace Gotcha.Core.Entities
         public string Notes { get; set; }
         public ICollection<TargetAssignment> TargetAssignments { get; set; }
 
+        /* Standard constructor
+         * Used when creating a new Player object when a User joins a game */
         public Player(User user, Game game)
         {
             _id = Guid.NewGuid();
@@ -50,6 +52,8 @@ namespace Gotcha.Core.Entities
             TargetAssignments = new List<TargetAssignment>();
         }
 
+        /* Constructor to set a custom username
+         * Only used if the game rules do not enforce real names */
         public Player(User user, Game game, string username) : this(user, game)
         {
             if (game.Rules.UseRealNames)
@@ -64,7 +68,9 @@ namespace Gotcha.Core.Entities
             }
         }
 
-        public Player(User user, Game game, string? username, string profileImageSource) : this(user, game)
+        /* Constructor to set a custom username (optionally) and profile image (optionally)
+         * Used when creating a new player using custom rules */
+        public Player(User user, Game game, string? username, string? profileImageSource) : this(user, game)
         {
             if (game.Rules.UseRealNames && username != null)
             {
@@ -81,6 +87,7 @@ namespace Gotcha.Core.Entities
                 _playerName = username;
             }
 
+            // checks are done in the property setter
             ProfileImageSource = profileImageSource;
         }
 
@@ -92,10 +99,6 @@ namespace Gotcha.Core.Entities
             _gameId = gameId;
             _game = game;
             _playerName = playerName;
-            if (game.Rules.EnforcePlayerImages && user.ProfileImageSource == null)
-            {
-                throw new Exception("This game requires players to have profile images, but the user does not have one set.");
-            }
             ProfileImageSource = profileImageSource;
             IsAlive = isAlive;
             Notes = notes;
@@ -142,6 +145,11 @@ namespace Gotcha.Core.Entities
                     throw new Exception("The game settings do not allow player images to be set.");
                 }
 
+                if (this.Game.Rules.EnforcePlayerImages && value == null)
+                {
+                    throw new Exception("This game requires players to have profile images, but the user does not have one set.");
+                }
+
                 if (ThirdLineValidationService.IsCleanProfileImageSource(value))
                 {
                     profileImageSource = value;
@@ -155,6 +163,58 @@ namespace Gotcha.Core.Entities
         public override string ToString()
         {
             return PlayerName;
+        }
+
+        public Player GetCurrentTarget()
+        {
+            TargetAssignment? currentAssignment = TargetAssignments
+                                        .FirstOrDefault(ta => ta.TargetId == this.Id && ta.AssignmentStatus == Enums.AssignmentStatus.Ongoing);
+
+            if (currentAssignment == null)
+            {
+                throw new InvalidOperationException("No ongoing target assignment found for this player.");
+            }
+
+            if(currentAssignment.Target == null)
+            {
+                throw new InvalidOperationException("The current target assignment does not have a valid target.");
+            }
+
+            return currentAssignment.Target;
+        }
+
+        public Player GetCurrentHunter()
+        {
+            TargetAssignment? currentAssignment = TargetAssignments
+                                        .FirstOrDefault(ta => ta.TargetId == this.Id && ta.AssignmentStatus == Enums.AssignmentStatus.Ongoing);
+            
+            if (currentAssignment == null)
+            {
+                throw new InvalidOperationException("No ongoing target assignment found for this player.");
+            }
+            if (currentAssignment.Hunter == null)
+            {
+                throw new InvalidOperationException("The current target assignment does not have a valid hunter.");
+            }
+            return currentAssignment.Hunter;
+        }
+
+        public List<Player> GetKilledPlayers()
+        {
+            List<Player> killedPlayers = TargetAssignments
+                                            .Where(ta => ta.Kill != null)
+                                            .Select(ta => ta.Target)
+                                            .ToList();
+
+            return killedPlayers;
+        }
+
+        public Player? GetKiller()
+        {
+            TargetAssignment? killAssignment = TargetAssignments
+                                     .FirstOrDefault(ta => ta.Kill != null);
+
+            return killAssignment?.Hunter;
         }
     }
 }
