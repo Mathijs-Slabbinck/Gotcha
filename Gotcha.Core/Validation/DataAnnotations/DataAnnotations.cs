98c6d8c0a4761c9351d77174c723b194a526b98d
Mathijs Slabbinck <mathijs.slabbinck@student-howest.be>
2025-12-19 21:40:25 +0100

    Fix exception handling & validation handling

---
+++ Gotcha.Core/Validation/DataAnnotations/DataAnnotations.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Configuration;
+
+namespace Gotcha.Core.Validation.DataAnnotations
+{
+    public class NotReservedUsernameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not string username)
+                return ValidationResult.Success!;
+
+            UsernameValidationService validationService = new UsernameValidationService(new ConfigurationBuilder().AddJsonFile("appsettings.json").Build());
+            if (validationService.IsReservedUsername(username))
+            {
+                return new ValidationResult(
+                    ErrorMessage ?? "This username is not allowed. Please choose another."
+                );
+            }
+
+            return ValidationResult.Success!;
+        }
+    }
+}
