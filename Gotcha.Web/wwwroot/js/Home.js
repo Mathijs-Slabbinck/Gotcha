e0fd7babd02eeb583b378c7652c76370708e4d29
Mathijs Slabbinck <mathijs.slabbinck@student-howest.be>
2026-02-14 21:30:06 +0100

    Minor bug fixes & start working on logged in index

---
+++ Gotcha.Web/wwwroot/js/Home.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+
+window.addEventListener("load", initialize);
+
+function initialize() {
+    const username = document.getElementById("username").innerText;
+    const spanUsernameInTitle = document.getElementById("userNameInTitle");
+    console.log(spanUsernameInTitle);
+    typeWriter(username, spanUsernameInTitle);
+}
+
+function typeWriter(text, field, speed = 80, i = 0) {
+    if (i === 0) {
+        field.innerHTML = '<span class="cursor"></span>';
+    }
+    if (i < text.length) {
+        field.innerHTML = text.slice(0, i + 1) + '<span class="cursor"></span>';
+        setTimeout(() => typeWriter(text, field, speed, i + 1), speed);
+    }
+    else {
+        setTimeout(function () {
+            field.innerHTML = field.innerHTML.replace('<span class="cursor"></span>', '');
+        }, 1500);
+    }
+}
