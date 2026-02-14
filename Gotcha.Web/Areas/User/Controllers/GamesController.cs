ec5b5c1a2cbe8fd5c9427deb3627dcb6dd499606
Mathijs Slabbinck <mathijs.slabbinck@student-howest.be>
2026-02-14 04:11:25 +0100

    Fix minor bugs

---
+++ Gotcha.Web/Areas/User/Controllers/GamesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gotcha.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
